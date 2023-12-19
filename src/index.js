4d1faecd514860643fc9c98594296ef5e8945502
khaledmustafa91 <khaled.mustafa@cis.asu.edu.eg>
2023-12-19 09:12:36 +0200

    Remove test and add styling for all website

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './css/index.css';
+// import './js/tiny-slider';
+// import './js/custom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
