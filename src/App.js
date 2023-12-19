325486a44b4ad2944a16ac3ce313404d54c306aa
khaledmustafa91 <khaled.mustafa@cis.asu.edu.eg>
2023-12-19 11:13:45 +0200

    Add services component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import NavBar from './components/NavBar';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Shop from './components/Shop';
 import AboutUs from './components/AboutUs';
+import Services from './components/Services';
 function App() {
   return (
     <Router>
@@ -13,6 +14,7 @@ function App() {
           <Route exact path="/" element={<Home />} />
           <Route exact path="/shop" element={<Shop />} />
           <Route exact path="/about_us" element={<AboutUs />} />
+          <Route exact path="/services" element={<Services />} />
         </Routes>
         <Footer />
       </div>
