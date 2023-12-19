b4d42a2999ef48a25e7db7a944eebb36a9525181
khaledmustafa91 <khaled.mustafa@cis.asu.edu.eg>
2023-12-19 09:24:14 +0200

    Add product list component

---
+++ src/components/ProductList.js
@@ -0,0 +1,41 @@
+import { Link } from 'react-router-dom';
+import CrossImage from '../assets/images/cross.svg';
+
+const ProductList = ({ products, HomePage }) => {
+  return (
+    products &&
+    products.map((product) => {
+      if (HomePage) {
+        return (
+          <div class="col-md-12 col-lg-3 mb-5 mb-lg-0">
+            <h2 class="mb-4 section-title">{product.title}</h2>
+            <p class="mb-4">{product.description}</p>
+            <p>
+              <Link to="/shop" className="btn">
+                Explore
+              </Link>
+            </p>
+          </div>
+        );
+      } else {
+        return (
+          <div className="col-12 col-md-4 col-lg-3 mb-5">
+            <a className="product-item" href="#">
+              <img
+                src={product.photo}
+                className="img-fluid product-thumbnail"
+              />
+              <h3 className="product-title">{product.title}</h3>
+              <strong className="product-price">${product.price}</strong>
+              <span className="icon-cross">
+                <img src={CrossImage} className="img-fluid" />
+              </span>
+            </a>
+          </div>
+        );
+      }
+    })
+  );
+};
+
+export default ProductList;
