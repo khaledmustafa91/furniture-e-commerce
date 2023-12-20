0cd15f3a25532736ce5d62d653e0b9ea64b23b36
khaledmustafa91 <khaled.mustafa@cis.asu.edu.eg>
2023-12-20 11:23:19 +0200

    remove import custom js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './css/index.css';
-import '../public/js/custom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
