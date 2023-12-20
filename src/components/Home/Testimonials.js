727274ec29b84fc5a96bb5c222078a251bc42d82
khaledmustafa91 <khaled.mustafa@cis.asu.edu.eg>
2023-12-20 11:17:39 +0200

    Add carousel

--- src/components/Home/Testimonials.js
+++ src/components/Home/Testimonials.js
@@ -1,4 +1,39 @@
+import 'react-responsive-carousel/lib/styles/carousel.min.css'; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
+
 const Testimonials = () => {
+  const carouselProp = {
+    autoPlay: false,
+    showArrows: true,
+    infiniteLoop: true,
+    showThumbs: false,
+    showStatus: false,
+    // renderIndicator: (clickHandler, hasPrev, label) => {
+    //   return <span className="fa fa-chevron-left" />;
+    // },
+    // renderArrowPrev: (clickHandler, hasPrev, label) => {
+    //   return (
+    //     <span
+    //       className="prev arrows"
+    //       data-controls="prev"
+    //       onClick={clickHandler}
+    //     >
+    //       <span className="fa fa-chevron-left" />
+    //     </span>
+    //   );
+    // },
+    // renderArrowNext: (clickHandler, hasNext, label) => {
+    //   return (
+    //     <span
+    //       className="next arrows"
+    //       data-controls="next"
+    //       onClick={clickHandler}
+    //     >
+    //       <span className="fa fa-chevron-right" />
+    //     </span>
+    //   );
+    // },
+  };
   return (
     <>
       {/* Start Testimonial Slider */}
@@ -11,117 +46,112 @@ const Testimonials = () => {
           </div>
           <div className="row justify-content-center">
             <div className="col-lg-12">
-              <div className="testimonial-slider-wrap text-center">
-                <div id="testimonial-nav">
-                  <span className="prev" data-controls="prev">
-                    <span className="fa fa-chevron-left" />
-                  </span>
-                  <span className="next" data-controls="next">
-                    <span className="fa fa-chevron-right" />
-                  </span>
+              <Carousel {...carouselProp}>
+                <div className="item">
+                  <div className="row justify-content-center">
+                    <div className="col-lg-8 mx-auto">
+                      <div className="testimonial-block text-center">
+                        <blockquote className="mb-5">
+                          <p>
+                            “Donec facilisis quam ut purus rutrum lobortis.
+                            Donec vitae odio quis nisl dapibus malesuada. Nullam
+                            ac aliquet velit. Aliquam vulputate velit imperdiet
+                            dolor tempor tristique. Pellentesque habitant morbi
+                            tristique senectus et netus et malesuada fames ac
+                            turpis egestas. Integer convallis volutpat dui quis
+                            scelerisque.”
+                          </p>
+                        </blockquote>
+                        <div className="author-info">
+                          <div className="author-pic">
+                            <img
+                              src={
+                                'https://res.cloudinary.com/dg7tijq2w/image/upload/v1702975891/Furniture-e-commerce/team/nahizdl9hs7d41pcv3cl.jpg'
+                              }
+                              alt="Maria Jones"
+                              className="img-fluid"
+                            />
+                          </div>
+                          <h3 className="font-weight-bold">Maria Jones</h3>
+                          <span className="position d-block mb-3">
+                            CEO, Co-Founder, XYZ Inc.
+                          </span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
                 </div>
-                <div className="testimonial-slider">
-                  <div className="item">
-                    <div className="row justify-content-center">
-                      <div className="col-lg-8 mx-auto">
-                        <div className="testimonial-block text-center">
-                          <blockquote className="mb-5">
-                            <p>
-                              “Donec facilisis quam ut purus rutrum lobortis.
-                              Donec vitae odio quis nisl dapibus malesuada.
-                              Nullam ac aliquet velit. Aliquam vulputate velit
-                              imperdiet dolor tempor tristique. Pellentesque
-                              habitant morbi tristique senectus et netus et
-                              malesuada fames ac turpis egestas. Integer
-                              convallis volutpat dui quis scelerisque.”
-                            </p>
-                          </blockquote>
-                          <div className="author-info">
-                            <div className="author-pic">
-                              <img
-                                src="images/person-1.png"
-                                alt="Maria Jones"
-                                className="img-fluid"
-                              />
-                            </div>
-                            <h3 className="font-weight-bold">Maria Jones</h3>
-                            <span className="position d-block mb-3">
-                              CEO, Co-Founder, XYZ Inc.
-                            </span>
+
+                <div className="item">
+                  <div className="row justify-content-center">
+                    <div className="col-lg-8 mx-auto">
+                      <div className="testimonial-block text-center">
+                        <blockquote className="mb-5">
+                          <p>
+                            “Donec facilisis quam ut purus rutrum lobortis.
+                            Donec vitae odio quis nisl dapibus malesuada. Nullam
+                            ac aliquet velit. Aliquam vulputate velit imperdiet
+                            dolor tempor tristique. Pellentesque habitant morbi
+                            tristique senectus et netus et malesuada fames ac
+                            turpis egestas. Integer convallis volutpat dui quis
+                            scelerisque.”
+                          </p>
+                        </blockquote>
+                        <div className="author-info">
+                          <div className="author-pic">
+                            <img
+                              src={
+                                'https://res.cloudinary.com/dg7tijq2w/image/upload/v1702975891/Furniture-e-commerce/team/b0dfqkghv1c62wr7ommy.jpg'
+                              }
+                              alt="Maria Jones"
+                              className="img-fluid"
+                            />
                           </div>
+                          <h3 className="font-weight-bold">Maria Jones</h3>
+                          <span className="position d-block mb-3">
+                            CEO, Co-Founder, XYZ Inc.
+                          </span>
                         </div>
                       </div>
                     </div>
                   </div>
-                  {/* END item */}
-                  <div className="item">
-                    <div className="row justify-content-center">
-                      <div className="col-lg-8 mx-auto">
-                        <div className="testimonial-block text-center">
-                          <blockquote className="mb-5">
-                            <p>
-                              “Donec facilisis quam ut purus rutrum lobortis.
-                              Donec vitae odio quis nisl dapibus malesuada.
-                              Nullam ac aliquet velit. Aliquam vulputate velit
-                              imperdiet dolor tempor tristique. Pellentesque
-                              habitant morbi tristique senectus et netus et
-                              malesuada fames ac turpis egestas. Integer
-                              convallis volutpat dui quis scelerisque.”
-                            </p>
-                          </blockquote>
-                          <div className="author-info">
-                            <div className="author-pic">
-                              <img
-                                src="images/person-1.png"
-                                alt="Maria Jones"
-                                className="img-fluid"
-                              />
-                            </div>
-                            <h3 className="font-weight-bold">Maria Jones</h3>
-                            <span className="position d-block mb-3">
-                              CEO, Co-Founder, XYZ Inc.
-                            </span>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                  {/* END item */}
-                  <div className="item">
-                    <div className="row justify-content-center">
-                      <div className="col-lg-8 mx-auto">
-                        <div className="testimonial-block text-center">
-                          <blockquote className="mb-5">
-                            <p>
-                              “Donec facilisis quam ut purus rutrum lobortis.
-                              Donec vitae odio quis nisl dapibus malesuada.
-                              Nullam ac aliquet velit. Aliquam vulputate velit
-                              imperdiet dolor tempor tristique. Pellentesque
-                              habitant morbi tristique senectus et netus et
-                              malesuada fames ac turpis egestas. Integer
-                              convallis volutpat dui quis scelerisque.”
-                            </p>
-                          </blockquote>
-                          <div className="author-info">
-                            <div className="author-pic">
-                              <img
-                                src="images/person-1.png"
-                                alt="Maria Jones"
-                                className="img-fluid"
-                              />
-                            </div>
-                            <h3 className="font-weight-bold">Maria Jones</h3>
-                            <span className="position d-block mb-3">
-                              CEO, Co-Founder, XYZ Inc.
-                            </span>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                  {/* END item */}
                 </div>
-              </div>
+
+                <div className="item">
+                  <div className="row justify-content-center">
+                    <div className="col-lg-8 mx-auto">
+                      <div className="testimonial-block text-center">
+                        <blockquote className="mb-5">
+                          <p>
+                            “Donec facilisis quam ut purus rutrum lobortis.
+                            Donec vitae odio quis nisl dapibus malesuada. Nullam
+                            ac aliquet velit. Aliquam vulputate velit imperdiet
+                            dolor tempor tristique. Pellentesque habitant morbi
+                            tristique senectus et netus et malesuada fames ac
+                            turpis egestas. Integer convallis volutpat dui quis
+                            scelerisque.”
+                          </p>
+                        </blockquote>
+                        <div className="author-info">
+                          <div className="author-pic">
+                            <img
+                              src={
+                                'https://res.cloudinary.com/dg7tijq2w/image/upload/v1702975891/Furniture-e-commerce/team/lqg5ygu91ymcxgkel2ha.jpg'
+                              }
+                              alt="Maria Jones"
+                              className="img-fluid"
+                            />
+                          </div>
+                          <h3 className="font-weight-bold">Maria Jones</h3>
+                          <span className="position d-block mb-3">
+                            CEO, Co-Founder, XYZ Inc.
+                          </span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </Carousel>
             </div>
           </div>
         </div>
