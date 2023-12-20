6c7dc2d7d6b8d1fb8937bd35179a8b8eaa64b8e1
khaledmustafa91 <khaled.mustafa@cis.asu.edu.eg>
2023-12-20 11:21:29 +0200

    import custom js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './css/index.css';
-// import './js/tiny-slider';
-// import './js/custom';
+import '../public/js/custom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
