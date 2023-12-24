cc4cb042e38efe3812e5ee97e1d6614b7a1ec44a
khaledmustafa91 <khaled.mustafa@cis.asu.edu.eg>
2023-12-24 12:45:25 +0200

    Generic the product list component

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,15 +1,60 @@
 import { Link } from 'react-router-dom';
 import CrossImage from '../assets/images/cross.svg';
+import { useContext } from 'react';
+import { CartContext } from '../App';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const ProductList = ({ products, HomePage }) => {
+  const { cart, setCart } = useContext(CartContext);
+
+  const MergeOldItems = (product_id) => {
+    return cart.map((item) => {
+      if (item.id === product_id) {
+        item.quantity += 1;
+      }
+      return item;
+    });
+  };
+
+  const handleAddItem = (product_id) => {
+    fetch(`http://localhost:4000/products/${product_id}`)
+      .then((res) => res.json())
+      .then((data) => {
+        const old_item = cart.find((item) => item.id === product_id);
+        let newCart = cart;
+        if (old_item) {
+          newCart = MergeOldItems(product_id);
+        } else {
+          data['quantity'] = 1;
+          cart.push(data);
+        }
+        setCart(newCart);
+        ToastAddedSuccessfully();
+      });
+  };
+
+  const ToastAddedSuccessfully = () => {
+    toast.success('Successfully added to your cart', {
+      position: 'top-right',
+      autoClose: 2000,
+      hideProgressBar: true,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: 'colored',
+    });
+  };
+
   return (
     products &&
     products.map((product) => {
       if (HomePage) {
         return (
-          <div class="col-12 col-md-6 col-lg-4 mb-4 mb-lg-0">
-            <h2 class="mb-4 section-title">{product.title}</h2>
-            <p class="mb-4">{product.description}</p>
+          <div className="col-12 col-md-6 col-lg-4 mb-4 mb-lg-0">
+            <h2 className="mb-4 section-title">{product.title}</h2>
+            <p className="mb-4">{product.description}</p>
             <p>
               <Link to="/shop" className="btn">
                 Explore
@@ -19,16 +64,21 @@ const ProductList = ({ products, HomePage }) => {
         );
       } else {
         return (
-          <div className="col-12 col-md-4 col-lg-3 mb-5">
-            <a className="product-item" href="#">
+          <div className="col-12 col-md-4 col-lg-3 mb-5" key={product.id}>
+            <a
+              onClick={() => handleAddItem(product.id)}
+              className="product-item"
+              href="#"
+            >
               <img
                 src={product.photo}
+                alt={`product-${product.id}`}
                 className="img-fluid product-thumbnail"
               />
               <h3 className="product-title">{product.title}</h3>
               <strong className="product-price">${product.price}</strong>
               <span className="icon-cross">
-                <img src={CrossImage} className="img-fluid" />
+                <img src={CrossImage} alt="cross" className="img-fluid" />
               </span>
             </a>
           </div>
